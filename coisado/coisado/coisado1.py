14a57dabfc53e26f219a51fcf353660f175671a4
jarufino <jarufino@bol.com.br>
2024-10-05 23:23:04 -0300

    Criação de pacote simples no python
    Após criação o nome do pacote é: coisado
    
    Foi só para aprendizagem

---
+++ coisado/coisado/coisado1.py
@@ -0,0 +1,9 @@
+def coisado():
+    print('Olá, como vão?')
+    print('Então, não conheço muito Python, estou disposto a aprender com vocês')
+    print('Tenho um sonho, que é o de trabalhar efetivamente como programador')
+    print('Mas conheço bem pouco, já fui estagiário em NOC aí na NTT Data')
+    print('Espero poder voltar, mas na área de dados como efetivo')
+    print('Grato')
+
+coisado()
